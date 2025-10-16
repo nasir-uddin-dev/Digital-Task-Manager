83a0a6359b26b1866980508258fed3106b2d39b4
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-16 20:42:53 +0600

    login provider

--- lib/ui/controllers/login_provider.dart
+++ lib/ui/controllers/login_provider.dart
@@ -1,13 +1,49 @@
-class  extends StatefulWidget {
-  const ({super.key});
+import 'package:flutter/foundation.dart';
+import 'package:task_management_app/data/models/user_model.dart';
+import 'package:task_management_app/data/services/api_caller.dart';
+import 'package:task_management_app/data/utils/urls.dart';
+import 'package:task_management_app/ui/controllers/auth_controller.dart';
 
-  @override
-  State<> createState() => _State();
-}
+class LoginProvider extends ChangeNotifier {
+  bool _loginInprogress = false;
 
-class _State extends State<> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  String? _errorMessage;
+
+  bool get loginInProgress => _loginInprogress; //getter method access private-variable
+  String? get errorMessage => _errorMessage;
+
+
+  Future<bool> login(String email, String password) async {
+    bool isSuccess = false;
+
+    _loginInprogress =
+        true; //when api will be called , then loading will be started
+    notifyListeners(); //showing for loading to UI, In that case, notifyListeners is used
+
+    //Making data prepare to key-value pair for sending request body to Api server
+    Map<String, dynamic> requestBody = {"email": email, "password": password};
+
+    //To send data server. This is a post type request
+    final ApiResponse response =
+        await ApiCaller.postRequest(url: Urls.loginUrl, body: requestBody);
+
+    //Here , There is a if-else condition. If condition is true, Then it is executed
+    //Otherwise , else part is executed
+    if (response.isSuccess && response.responseData['status'] == 'success') {
+      UserModel model = UserModel.fromJson(response.responseData['data']);  //we will collect data from server and covert it Json to objcet
+      String token = response.responseData['token'];  //Token is being taken from sever
+
+      await AuthController.saveUserData(model, token);  //SharedPreference is saved data
+
+      _errorMessage = null;
+      isSuccess = true;
+    } else {
+      _errorMessage = response.errorMessage;
+    }
+
+    _loginInprogress = false;
+    notifyListeners();
+
+    return isSuccess;
   }
 }
